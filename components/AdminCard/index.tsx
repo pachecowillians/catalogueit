4036057a3fefb73c6fb3b62c3d2172023fa99124
Willian Pacheco <willianpacheco31@gmail.com>
2021-06-15 16:12:53 -0300

    Create AdminCard component

--- components/AdminCard/index.tsx
+++ components/AdminCard/index.tsx
@@ -1,15 +1,18 @@
-import { Container } from './style'
-import Link from 'next/link'
+import { Buttons, Container } from './style'
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function AdminCard({ item }) {
     return (
-        <Link href={`/item/${item.id}`}>
-            <Container>
-                <img src={item.img} alt="Product" />
-                <span>{item.description}</span>
-                <span>R$ {item.price}</span>
-            </Container>
-        </Link>
+        <Container>
+            <img src={item.img} alt="Product" />
+            <span>{item.description}</span>
+            <span>R$ {item.price}</span>
+            <Buttons>
+                <button><EditIcon /></button>
+                <button><DeleteIcon /></button>
+            </Buttons>
+        </Container>
     );
 }
 
--- components/AdminCard/style.tsx
+++ components/AdminCard/style.tsx
@@ -2,14 +2,13 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     width: 300px;
-    height: 400px;
+    height: 425px;
     background-color: #fff;
     border-radius: 5px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
-    cursor: pointer;
     padding: 30px 20px;
     transform: scale(0.9);
     transition: all ease 0.2s;
@@ -30,5 +29,34 @@ export const Container = styled.div`
 
     &:hover{
         transform: scale(0.95);
+    }
+`;
+
+export const Buttons = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 50%;
+
+    button{
+        border: none;
+        outline: none;
+        background-color: inherit;
+
+        svg{
+            font-size: 2.5rem;
+            cursor: pointer;
+
+            &:hover{
+                filter: brightness(0.95);
+            }
+        }
+
+        &:first-child{
+            color: #6272a4	;
+        }
+        &:nth-child(2){
+            color: #ff5555;
+        }
     }
 `;
