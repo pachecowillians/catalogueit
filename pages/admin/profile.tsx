d3e71db7f17b93c209928dfdb2bd9f1ec28f9985
Willian Pacheco <willianpacheco31@gmail.com>
2021-06-15 10:23:05 -0300

    Create states for inputs

--- pages/admin/profile.tsx
+++ pages/admin/profile.tsx
@@ -1,8 +1,46 @@
 import Head from 'next/head'
 import styles from '../../styles/AdminProfile.module.css'
 import EditIcon from '@material-ui/icons/Edit';
+import { useState } from 'react';
 
 function Profile() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [profilePicture, setProfilePicture] = useState('');
+
+    function handleOnChangeUsername(event) {
+        setUsername(event.target.value);
+    }
+
+    function handleOnChangePassword(event) {
+        setPassword(event.target.value);
+    }
+
+    function handleOnChangeEmail(event) {
+        setEmail(event.target.value);
+    }
+
+    function handleOnChangePhone(event) {
+        setPhone(event.target.value);
+    }
+
+    function handleOnChangeProfilePicture() {
+        var file = document.querySelector('input[type=file]').files[0];
+        var reader = new FileReader();
+
+        reader.onloadend = function () {
+            setProfilePicture(reader.result);
+        }
+
+        if (file) {
+            reader.readAsDataURL(file);
+        } else {
+            setProfilePicture('');
+        }
+    }
+
     return (
         <>
             <Head>
@@ -13,18 +51,18 @@ function Profile() {
                 <div className={styles.body}>
                     <div className={styles.profilePicture}>
                         <label htmlFor="file">
-                            <img src="/vercel.svg" alt="Profile picture" />
+                            <img src={(profilePicture.length == 0) ? "/vercel.svg" : profilePicture} alt="Profile picture" />
                             <div className={styles.editImage}>
                                 <EditIcon style={{ fontSize: 25 }} />
                             </div>
                         </label>
                     </div>
                     <div className={styles.inputs}>
-                        <input type="text" placeholder="Username" />
-                        <input type="password" placeholder="Password" />
-                        <input type="email" placeholder="Email" />
-                        <input type="text" placeholder="Phone" />
-                        <input type="file" id="file" hidden />
+                        <input type="text" placeholder="Username" onChange={handleOnChangeUsername} value={username} />
+                        <input type="password" placeholder="Password" onChange={handleOnChangePassword} value={password} />
+                        <input type="email" placeholder="Email" onChange={handleOnChangeEmail} value={email} />
+                        <input type="text" placeholder="Phone" onChange={handleOnChangePhone} value={phone} />
+                        <input type="file" id="file" hidden onChange={handleOnChangeProfilePicture} />
                     </div>
                     <button>Save</button>
                 </div>
