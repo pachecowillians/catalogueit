29874ee2589a480add7d808c55dd9c93e82c811d
Willian Pacheco Silva <willianpacheco31@gmail.com>
2021-10-28 21:14:22 -0300

    Change AdminCard icons

--- components/AdminCard/index.tsx
+++ components/AdminCard/index.tsx
@@ -1,6 +1,7 @@
 import { Buttons, Container } from './style'
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
+import { IconButton } from '@material-ui/core';
 
 function AdminCard({ item, openModal }) {
     return (
@@ -9,8 +10,16 @@ function AdminCard({ item, openModal }) {
             <span>{item.description}</span>
             <span>R$ {item.price}</span>
             <Buttons>
-                <button onClick={() => { openModal(item) }}><EditIcon /></button>
-                <button><DeleteIcon /></button>
+                <label htmlFor="icon-button-file">
+                    <IconButton color="primary" aria-label="upload picture" component="span">
+                        <EditIcon fontSize='large' onClick={() => { openModal(item) }} />
+                    </IconButton>
+                </label>
+                <label htmlFor="icon-button-file">
+                    <IconButton color="secondary" aria-label="upload picture" component="span">
+                        <DeleteIcon fontSize='large' />
+                    </IconButton>
+                </label>
             </Buttons>
         </Container>
     );
